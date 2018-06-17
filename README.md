cd8b3790878f428b640fffa69da4cb4c48eea356
Claudio Agosti <claudio@tracking.exposed>
2018-06-17 04:01:41 +0200

    new pattern configured

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-experiment for https://fuckoffgoogle.de
+# For a cleaner web ecosystem
+
+1. Education through visualization: this project display invisible trackers and put name nearby
+2. Replacements: technical information on which replacement you can run
+3. Ecosystem: how much being surveilled play a difference? Is it necessary? we don't know, it should be 0, we'll cover that too.
+
+This is a campaign develop by side of [fuckoffgoogle](https://fuckoffgoogle.de)
+
+### technical note
+
+This campaign run with the [invi.sible.link](https://invi.sible.link) framework, without all the `npm i`, is:
+
+```
+git clone https://github.com/vecna/invi.sible.link.git
+cd invi.sible.link/campaign
+git clone https://github.com/vecna/krgotrex.git 
+cd ..
+campaign=krgotrex npm run v3-social-pressure
+```
---
+++ page/controller.js
@@ -0,0 +1,82 @@
+var _ = require('lodash');
+var debug = require('debug')('krgotrex:controller');
+var pug = require('pug');
+var nconf = require('nconf').env();
+var moment = require('moment');
+var Promise = require('bluebird');
+
+var mongo = require('../../../lib/mongo');
+var various = require('../../../lib/various');
+var getGooglesOnly = require('../../../routes/getGooglesOnly');
+
+function rootpage(req) {
+
+    debug("welcome to the rootpage");
+
+    return getGooglesOnly(_.set({}, 'params.campaign', 'kzbrg'))
+        .then(function(j) {
+
+            /* this is the format required in the .pug: 
+            results: [{
+                'href':
+                'description':
+                'googles': [{
+                    'productName': 'Google Analytics',
+                    'amount': 3
+                }]
+                'nothing':
+            }]
+            we are getting a collection you can see from
+            https://invi.sible.link/api/v1/google/kzbrg
+             ** ** ** ** ** ** ** ** ** ** ** ** ** ** */
+
+            var products = _.compact(_.uniq(_.map(j.json, 'product')));
+            var tested = _.uniq(_.map(j.json, 'href'));
+            var ret = _.reduce(j.json, function(memo, entry) {
+                _.set(memo, entry.description, entry.href);
+                return memo;
+            }, {});
+
+            ret = _.map(ret, function(href, description) {
+                return {
+                    href: href,
+                    description: description,
+                    googles: [],
+                    nothing: false
+                };
+            });
+
+            debug("%d tested site, %d unique Google services", _.size(tested), _.size(products));
+            _.each(tested, function(s) {
+                var isClean = true;
+                _.each(products, function(p) {
+                    var evidences = _.filter(j.json, { product: p, href: s });
+
+                    if(_.size(evidences)) {
+                        _.find(ret, { href: s }).googles.push({
+                            productName: p,
+                            amount: _.size(evidences)
+                        });
+                        isClean = false;
+                    }
+                });
+
+                if(isClean)
+                    _.find(ret, {href: s}).nothing = true;
+
+            }); 
+
+
+            return { 'text': 
+                pug.compileFile(
+                    __dirname + '/content.pug', {
+                        pretty: true,
+                        debug: false
+                    }
+                )({ results: _.shuffle(ret) })
+            };
+        });
+
+};
+
+module.exports = rootpage;
