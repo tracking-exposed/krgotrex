df78e3d6a2e3ce10f8362fa2f4cc2886d0e8d22c
Groschenroman <roman.hanig@posteo.de>
2018-07-13 14:38:29 +0200

    -S

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
-const connect = require('connect');
-const sassMiddleware = require('node-sass-middleware');
-const postcssMiddleware = require('postcss-middleware');
-const autoprefixer = require('autoprefixer');
-const path = require('path');
+var connect = require('connect');
+var sassMiddleware = require('node-sass-middleware');
+var postcssMiddleware = require('postcss-middleware');
+var autoprefixer = require('autoprefixer');
+var path = require('path');
 
-const srcPath = __dirname + '/page/styles';
-const destPath = __dirname + '/page';
+var srcPath = __dirname + '/page/styles';
+var destPath = __dirname + '/page';
 
-const serveStatic = require('serve-static')
-const http = require('http');
-const port = process.env.PORT || 8000;
-const app = connect();
+var serveStatic = require('serve-static')
+var http = require('http');
+var port = process.env.PORT || 8000;
+var app = connect();
 
-app.use('/', sassMiddleware({
+app
+.use('/', sassMiddleware({
   src: srcPath,
   dest: destPath,
   debug: true,
   outputStyle: 'compressed',
   force: true,
-}));
-// app.use('/', postcssMiddleware({
-//   plugins: [
-//     /* Plugins */
-//     autoprefixer({
-//       /* Options */
-//     })
-//   ],
-//   src: function(req) {
-//     return path.join(destPath, req.url);
-//   }
-// }));
-app.use('/', 
+}))
+.use('/page', postcssMiddleware({
+  plugins: [
+    /* Plugins */
+    autoprefixer({
+      /* Options */
+    })
+  ],
+  src: function(req) {
+    return path.join(destPath, req.url);
+  }
+})).use('/', 
   serveStatic('./page', {})
 );
+
 http.createServer(app).listen(port);
 console.log('Server listening on port ' + port);
 console.log('srcPath is ' + srcPath);
