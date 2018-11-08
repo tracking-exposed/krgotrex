d23c22aa0646507cb34dfbd4001326b8f10e0196
Claudio invi.sible.link <claudio@tracking.exposed>
2018-11-08 15:59:25 +0000

    now the API are returning multiple chunks

--- site/js/services/data.service.js
+++ site/js/services/data.service.js
@@ -17,18 +17,17 @@ let services = {};
 services.getAnalyzedSites = new Promise((resolve, reject) => {
   https.get(analyzedSitesUrl, (response) => {
     let sites = [];
+    let body = '';
     // Get the data as utf8 strings.
     // If an encoding is not set, Buffer objects will be received.
     response.setEncoding('utf8');
 
     // Readable streams emit 'data' events once a listener is added
     response.on('data', (chunk) => {
-      let body = '';
-      // Ensure we get a stringified JSON
-      body += chunk instanceof Buffer
-            ? chunk.toJSON()
-            : JSON.stringify(eval(`(${chunk})`));
+      body += chunk;
+    });
 
+    response.on('end', () => {
       // Convert json Object into an iterable array
       const sitesJson = JSON.parse(body);
       sitesJson.forEach(site => {
@@ -52,18 +51,17 @@ services.getAnalyzedSites = new Promise((resolve, reject) => {
 services.getExposureResults = new Promise((resolve, reject) => {
   https.get(exposureResultsUrl, (response) => {
     let results = [];
+    let body = '';
     // Get the data as utf8 strings.
     // If an encoding is not set, Buffer objects will be received.
     response.setEncoding('utf8');
 
     // Readable streams emit 'data' events once a listener is added
     response.on('data', (chunk) => {
-      let body = '';
-      // Ensure we get a stringified JSON
-      body += chunk instanceof Buffer
-            ? chunk.toJSON()
-            : JSON.stringify(eval(`(${chunk})`));
+      body += chunk;
+    });
 
+    response.on('end', () => {
       // Convert json Object into an iterable array
       const resultsJson = JSON.parse(body);
       resultsJson.forEach(result => {
