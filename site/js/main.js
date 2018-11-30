66ba1fe9869fce7140796d898f8ed5278400b7b5
Groschenroman <roman.hanig@posteo.de>
2018-11-30 16:09:04 +0100

    Update main js file

--- site/js/main.js
+++ site/js/main.js
@@ -1,5 +1,86 @@
 "use strict";
 
+// Providers
+const singleCachedSiteUrl = 'https://kreuzberg.google.tracking.exposed/api/v1/site/',
+      singleCheckSiteUrl = 'https://kreuzberg.google.tracking.exposed/api/v1/monosite/';
+
+let siteToCheck = '';
+
+async function getSingleSite(siteName) {
+  if (!siteName || (siteName && siteToCheck.length && siteToCheck === siteName)) {
+    return;
+  }
+  return await new Promise((resolve, reject) => {
+    siteToCheck = siteName;
+    $.getJSON(singleCachedSiteUrl + siteName, (response) => {
+      if (!response.when) {
+        $.getJSON(singleCheckSiteUrl + siteName, (response) => {
+          resolve(response);
+        });
+      } else {
+        resolve(response);
+      }
+    })
+    .fail((jqXHR, textStatus, errorThrown) => {
+      const reason = `getJSON request failed in getSingleSite()! \nDetails: ${textStatus} \n${errorThrown} \n${JSON.stringify(jqXHR)}`;
+      reject(reason);
+    });
+  });
+}
+
+/**
+ * C H E C K  Y O U R  S I T E
+ */
+
+// helpers
+function disableFormCheckSite(disable = true) {
+  $('#form-check-site-input').prop("disabled", disable);
+  $('#form-check-site-btn').prop("disabled", disable);
+}
+async function checkSite(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  const inputVal = event.target[1].value;
+  // Display loading indicator
+  $('#check-site-result').empty();
+  $('#check-site-loader').show();
+  disableFormCheckSite();
+  try {
+    const regex = new RegExp(/^https?:\/\/|\s/, 'gi'),
+      trimmedVal = inputVal.replace(regex, ''); // Remove 'http(s)://' as well as white space
+    const response = await getSingleSite(trimmedVal);
+    let result = {};
+    if (response.summary.googles) {
+      result = {
+        page: response.page,
+        googles: Object.keys(response.summary.googles).map(key => `<li class="small-8 large-10 column">${key}</li>`) + 
+          Object.values(response.summary.googles).map(value => `<li class="small-4 large-2 column">${value}</li>`)
+      };
+      console.log(result);
+    }
+    $('#check-site-loader').hide();
+    $('#check-site-result').append(`
+      <h2>${result.page}</h2>
+      <ul class="site-googles">${result.googles}</ul>
+    `);
+    disableFormCheckSite(false);
+  } catch (error) {
+    $('#check-site-loader').hide();
+    disableFormCheckSite(false);
+    console.error('main.js: Site could not be checked. Reason: ', error);
+  }
+}
+
+function searchSite(event) {
+  if (!event) return;
+
+  event.preventDefault();
+  console.log(event);
+  // const resultSite = sitesArray.search(e.target.value) > -1;
+  // console.log(resultSite);
+  // return resultSite;
+}
+
 $(function() {
 
   function setActiveLinkClass() {
