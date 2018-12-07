e5d132ed9bee8bc6c7a03e5104af2732d03e32d6
Groschenroman <roman.hanig@posteo.de>
2018-12-07 17:18:28 +0100

    Minor keyword upgrade

--- site/js/main.js
+++ site/js/main.js
@@ -159,12 +159,12 @@ $searchField.on('keyup', debounce((event) => {
 
 // debounce so filtering doesn't happen every millisecond
 function debounce( fn, threshold ) {
-  var timeout;
+  let timeout;
   threshold = threshold || 100;
   return function debounced() {
     clearTimeout( timeout );
-    var args = arguments;
-    var _this = this;
+    const args = arguments;
+    const _this = this;
     function delayed() {
       fn.apply( _this, args );
     }
